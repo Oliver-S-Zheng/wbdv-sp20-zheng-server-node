d61b324c64d9dd8a655318803abb02dc157e31ed
Oliver Zheng <oliverzheng07@gmail.com>
2020-04-10 13:47:10 -0400

    Quiz service and quiz controller implementation

--- controllers/quiz.controller.server.js
+++ controllers/quiz.controller.server.js
@@ -1,21 +1,13 @@
+const quizService = require('../services/quiz.service.server')
+
 module.exports = function(app) {
-
-    const quizzes = [
-        {_id: '123', title: 'Quiz 1'},
-        {_id: '234', title: 'Quiz 2'},
-        {_id: '345', title: 'Quiz 3'}
-
-    ]
-
-    app.get('/hello', (req, res) =>
-        res.send('Hello World'))
-
     app.get('/api/quizzes', (req, res) =>
-        res.send(quizzes))
+        res.send(quizService.findAllQuizzes()))
 
     app.get('/api/quizzes/:qzid', (req, res) =>
-        res.json(quizzes
-            .find(quiz =>
-                quiz._id === req.params['qzid'])))
+    {
+        const quizId = req.params['qzid']
+        res.json(quizService.findQuizById(quizId))
+    })
 
 }
